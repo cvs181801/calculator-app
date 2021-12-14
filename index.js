fa3e9d1b7a3a3bcb3820d84b1b807755685a0796
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-14 08:29:51 -0800

    create a way to sum two integers

---
+++ index.js
@@ -0,0 +1,38 @@
+const oneBtn = document.getElementById("1");
+const twoBtn = document.getElementById("2");
+const threeBtn = document.getElementById("3");
+const addBtn = document.getElementById("+");
+const equalsBtn = document.getElementById("=");
+const display = document.querySelector(".display__inner");
+
+oneBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    display.textContent += 1;
+})
+
+twoBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    display.textContent += 2;
+})
+
+addBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    display.textContent += "+";
+})
+
+equalsBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    if(display.textContent.includes("+"))  {
+        display.textContent += "= "
+        let sum = add(parseInt(display.textContent[0]),parseInt(display.textContent[2]))
+        display.textContent += sum;
+    } else {
+        console.log("try again")
+    }
+})
+
+function add(value1, value2) {
+    return value1 + value2;
+}
+
+
